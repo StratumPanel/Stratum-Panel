ff5e99e1b504a57220ffce9de27421b8a18ec887
Eric Wang <msexploiting@gmail.com>
2021-05-24 20:12:00 -0500

    Updated editor config

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,20 +1,27 @@
 // @ts-nocheck
-require('./bootstrap');
+require('./bootstrap')
 
 // Import modules...
-import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { createApp, h } from 'vue'
+import {
+  App as InertiaApp,
+  plugin as InertiaPlugin,
+} from '@inertiajs/inertia-vue3'
+import NProgress from 'nprogress'
+import { Inertia } from '@inertiajs/inertia'
 
-const el = document.getElementById('app');
+const el = document.getElementById('app')
 
 createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
+  render: () =>
+    h(InertiaApp, {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: (name) => require(`./Pages/${name}`).default,
+    }),
 })
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .mount(el);
+  .mixin({ methods: { route } })
+  .use(InertiaPlugin)
+  .mount(el)
 
+Inertia.on('start', () => NProgress.start())
+Inertia.on('finish', () => NProgress.done())
