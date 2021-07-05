538b009ebe9be32e003fb7ee61b744a69160a150
Eric W <eric.wang401@gmail.com>
2021-07-05 15:00:12 -0500

    Added ServerPolicy for authorizing ownership

--- routes/client.php
+++ routes/client.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\Client\Servers\ServerController;
+use App\Policies\ServerPolicy;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/dashboard', [ServerController::class, 'index'])->name('dashboard');
 
 
-Route::group(['prefix' => '/servers'], function () {
+Route::group(['prefix' => '/servers', 'middleware' => 'can:view,server'], function () {
     Route::get('/{server}', [ServerController::class, 'show'])->name('servers.show');
 });
