c026292febc6c38fa5654b2fd970d0ddbfe5733a
Eric W <eric.wang401@gmail.com>
2021-07-12 16:30:55 -0500

    Updated PHPDOC

--- app/Services/Servers/StatusService.php
+++ app/Services/Servers/StatusService.php
@@ -6,12 +6,26 @@ use App\Models\Server;
 use App\Services\ProxmoxService;
 use proxmox\pve as Proxmox;
 
+/**
+ * Class StatusService
+ * @package App\Services\Servers
+ */
 class StatusService extends ProxmoxService
 {
+    /**
+     * StatusService constructor.
+     * @param Server $server
+     * @param Proxmox $pve
+     */
     public function __construct(private Server $server, private Proxmox $pve)
     {
     }
 
+    /**
+     * @param string $node
+     * @param string $vmid
+     * @return mixed
+     */
     public function fetchStatus(string $node, string $vmid)
     {
         return $this->proxmox->nodes()->node($node)->qemu()->vmid($vmid)->status()->current();
