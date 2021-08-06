598ee56d062a0000057197a49a10e6b9b91889a2
Eric Wang <eric.wang401@gmail.com>
2021-08-05 21:55:54 -0500

    Adjusted header bg to be more cohesive

--- resources/js/components/UserHeader.vue
+++ resources/js/components/UserHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-50">
+  <div class="bg-gray-100">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <div class="flex items-center">
         <div class="mr-4">
