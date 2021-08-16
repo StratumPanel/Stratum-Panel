e1c88f68fdd461defe0b805446129361aa099577
Eric Wang <eric.wang401@gmail.com>
2021-08-15 21:41:28 -0500

    REVERTING TIME AGAIN

--- resources/js/Pages/Servers/General.vue
+++ resources/js/Pages/Servers/General.vue
@@ -11,12 +11,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ServerLayout from '@/Layouts/ServerLayout.vue'
-<<<<<<< HEAD
-import SettingContainer from '@components/SettingContainer.vue'
-import { faBolt } from '@fortawesome/free-solid-svg-icons'
-=======
-
->>>>>>> parent of 975a61e (Added SettingContainer on frontend)
 export default defineComponent({
   name: 'General',
   components: {
