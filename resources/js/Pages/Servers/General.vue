4641056134ab92b1dfab5d0cd83027f37d672be7
Eric Wang <eric.wang401@gmail.com>
2021-08-15 22:03:55 -0500

    Added SettingLink component again

--- resources/js/Pages/Servers/General.vue
+++ resources/js/Pages/Servers/General.vue
@@ -1,25 +1,19 @@
 <template>
   <server-layout>
-    <template #title>
-      General
-    </template>
+    <template #title> General </template>
 
     <div class="grid gap-4 grid-cols-3">
       <setting-container
+        v-for="index in 3"
+        :key="index"
         :icon="faBolt"
         title="Power Options"
         description="Send commands to manage the state of your instance."
-      />
-      <setting-container
-        :icon="faBolt"
-        title="Power Options"
-        description="Send commands to manage the state of your instance."
-      />
-      <setting-container
-        :icon="faBolt"
-        title="Power Options"
-        description="Send commands to manage the state of your instance."
-      />
+      >
+        <template #actions>
+          <setting-link>Shutdown</setting-link>
+        </template>
+      </setting-container>
     </div>
   </server-layout>
 </template>
@@ -29,12 +23,13 @@ import { defineComponent } from 'vue'
 import ServerLayout from '@/Layouts/ServerLayout.vue'
 import SettingContainer from '@components/SettingContainer.vue'
 import { faBolt } from '@fortawesome/free-solid-svg-icons'
-
+import SettingLink from '@components/SettingLink.vue'
 export default defineComponent({
   name: 'General',
   components: {
     ServerLayout,
     SettingContainer,
+    SettingLink,
   },
   setup() {
     return { faBolt }
