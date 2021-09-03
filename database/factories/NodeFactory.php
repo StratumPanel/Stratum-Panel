2173c284ef24119288b065f79a7dffede30910f3
Eric Wang <eric.wang401@gmail.com>
2021-09-03 18:27:52 -0500

    Add Laradock

--- database/factories/NodeFactory.php
+++ database/factories/NodeFactory.php
@@ -27,7 +27,7 @@ class NodeFactory extends Factory
             'username' => $this->faker->name,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'port' => 69,
-            'auth_type' => 'PAM'
+            'auth_type' => 'pam'
         ];
     }
 }
