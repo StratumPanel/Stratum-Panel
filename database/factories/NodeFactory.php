330c16865612e18e4e02056d8e7f782f5978471c
Eric Wang <eric.wang401@gmail.com>
2021-12-22 00:50:50 +0100

    Update NodeFactory

--- database/factories/NodeFactory.php
+++ database/factories/NodeFactory.php
@@ -26,7 +26,7 @@ class NodeFactory extends Factory
             'hostname' => $this->faker->lastName(),
             'username' => $this->faker->name,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'port' => 69,
+            'port' => 8006,
             'auth_type' => 'pam'
         ];
     }
