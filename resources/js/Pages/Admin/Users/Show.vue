3dfa935a26c71613d342e6a994700098b297a827
Eric Wang <eric.wang401@gmail.com>
2022-05-29 06:31:26 +0200

    Add update profile page

--- resources/js/Pages/Admin/Users/Show.vue
+++ resources/js/Pages/Admin/Users/Show.vue
@@ -1,41 +1,44 @@
 <template>
-  <jet-form-section @submitted="updateProfile">
-    <template #title> Update Profile </template>
+  <app-layout>
+    <jet-form-section @submitted="updateProfile">
+      <template #title> Update Profile </template>
 
-    <template #description>
-      Ensure your account is using a long, random password to stay secure.
-    </template>
+      <template #description>
+        Ensure your account is using a long, random password to stay secure.
+      </template>
 
-    <template #form>
-      <div class="col-span-6 sm:col-span-4">
-        <jet-label for="name" value="Name" />
-        <jet-input
-          id="name"
-          type="password"
-          class="mt-1 block w-full"
-          v-model="form.name"
-          autocomplete="name"
-        />
-        <jet-input-error :message="form.errors.name" class="mt-2" />
-      </div>
-    </template>
+      <template #form>
+        <div class="col-span-6 sm:col-span-4">
+          <jet-label for="name" value="Name" />
+          <jet-input
+            id="name"
+            type="password"
+            class="mt-1 block w-full"
+            v-model="form.name"
+            autocomplete="name"
+          />
+          <jet-input-error :message="form.errors.name" class="mt-2" />
+        </div>
+      </template>
 
-    <template #actions>
-      <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-        Saved.
-      </jet-action-message>
+      <template #actions>
+        <jet-action-message :on="form.recentlySuccessful" class="mr-3">
+          Saved.
+        </jet-action-message>
 
-      <jet-button
-        :class="{ 'opacity-25': form.processing }"
-        :disabled="form.processing"
-      >
-        Save
-      </jet-button>
-    </template>
-  </jet-form-section>
+        <jet-button
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Save
+        </jet-button>
+      </template>
+    </jet-form-section>
+  </app-layout>
 </template>
 
 <script setup lang="ts">
+import AppLayout from '@layouts/AppLayout.vue'
 import JetActionMessage from '@/Jetstream/ActionMessage.vue'
 import JetButton from '@/Jetstream/Button.vue'
 import JetFormSection from '@/Jetstream/FormSection.vue'
@@ -45,10 +48,10 @@ import JetLabel from '@/Jetstream/Label.vue'
 import { useForm } from '@inertiajs/inertia-vue3'
 
 const form = useForm({
-    name: ''
+  name: '',
 })
 
 const updateProfile = () => {
-    alert('hi')
+  alert('hi')
 }
 </script>
